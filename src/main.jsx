6392d7e25d03536487433ed12ddf22765ccd379c
Gourav Kumar Pandit <117534016+GouravKumarPandit@users.noreply.github.com>
2024-08-16 23:36:54 +0530

    Route implemented with createRoutesFromElements

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Layout from './Layout.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Home from './Components/Home/Home.jsx'
 import About from './Components/About/About.jsx'
 import Contact from './Components/Contact/Contact.jsx'
 
-const router = createBrowserRouter([
+// First way to create routes in React JS 
+/* const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Layout />,
@@ -26,7 +27,17 @@ const router = createBrowserRouter([
 			},
 		]
 	},
-])
+]) */
+
+// Another way to create the routes in React JS 
+const router = createBrowserRouter(createRoutesFromElements(
+	<Route path='' element={<Layout />}>
+		{/* All these below files will go in place of Outlet */}
+		<Route path='' element={<Home />} />
+		<Route path='about-us' element={<About />} />
+		<Route path='contact-us' element={<Contact />} />
+	</Route>
+));
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
